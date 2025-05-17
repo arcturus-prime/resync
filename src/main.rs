1cfdd5ab63ab13a6805ffe0fa56ce595003d333b
Seanan Scott <seanans135@gmail.com>
2025-05-16 21:39:31 -0500

    Refactor UI into components

--- src/main.rs
+++ src/main.rs
@@ -1,174 +1,38 @@
 mod error;
 mod net;
 mod project;
+mod ui;
 
-use std::{
-    net::{Ipv4Addr, SocketAddrV4},
-    str::FromStr,
-};
+use eframe::egui::{ViewportBuilder, CentralPanel, Context, Ui};
 
-use eframe::egui::{self, Ui};
-use net::Client;
-use rfd::FileDialog;
+use project::ProjectListing;
 
-use project::{Project, ProjectData, ProjectKind};
+pub trait Widget {
+    fn render(&mut self, ui: &mut Ui);
+}
 
-#[derive(PartialEq)]
-enum Focus {
-    Open,
-    View,
+pub trait UpdateWidget<'a> {
+    type State;
+
+    fn render(&'a mut self, ui: &mut Ui, state: Self::State);
 }
 
 struct App {
-    tabs: Vec<Project>,
-    current: usize,
-
-    clipboard: ProjectData,
-
-    ip_text: String,
-    port_text: String,
-
-    focus: Focus,
+    projects: ProjectListing,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
-            tabs: vec![],
-            current: 0,
-
-            clipboard: ProjectData::new(),
-
-            ip_text: String::new(),
-            port_text: String::new(),
-
-            focus: Focus::View,
+            projects: ProjectListing::default(),
         }
-    }
-}
-
-impl App {
-    fn handle_add_project_window(&mut self, ui: &mut Ui) {
-        //Open project from file
-        if ui.button("Open").clicked() {
-            let Some(file) = FileDialog::new().pick_file() else {
-                return;
-            };
-
-            let Some(filename) = file.file_name() else {
-                return;
-            };
-            let filename = filename.to_string_lossy().to_string();
-
-            let project = match Project::new(ProjectKind::Local(file), filename) {
-                Ok(project) => project,
-                Err(e) => {
-                    println!("{e}");
-                    return;
-                }
-            };
-
-            self.tabs.push(project);
-            self.focus = Focus::View;
-        }
-
-        //Create new project
-        ui.add_space(5.0);
-
-        if ui.button("New").clicked() {
-            let Some(file) = FileDialog::new().save_file() else {
-                return;
-            };
-
-            let Some(filename) = file.file_name() else {
-                return;
-            };
-            let filename = filename.to_string_lossy().to_string();
-
-            let Ok(project) = Project::new(ProjectKind::Local(file), filename) else {
-                return;
-            };
-
-            self.tabs.push(project);
-            self.focus = Focus::View;
-        }
-
-        // Open project with connection to client
-        ui.add_space(15.0);
-
-        let ip_label = ui.label("IP Address:");
-        ui.add(egui::TextEdit::singleline(&mut self.ip_text).hint_text("127.0.0.1"))
-            .labelled_by(ip_label.id);
-
-        let port_label = ui.label("Port:");
-        ui.add(egui::TextEdit::singleline(&mut self.port_text).hint_text("12007"))
-            .labelled_by(port_label.id);
-
-        if ui.button("Connect").clicked() {
-            let ip = Ipv4Addr::from_str(&self.ip_text).unwrap_or(Ipv4Addr::new(127, 0, 0, 1));
-            let port = u16::from_str(&self.port_text).unwrap_or(12007);
-
-            let Ok(client) = Client::connect(SocketAddrV4::new(ip, port)) else {
-                return;
-            };
-
-            let Ok(project) = Project::new(ProjectKind::Remote(client), self.ip_text.clone())
-            else {
-                return;
-            };
-
-            self.tabs.push(project);
-            self.focus = Focus::View;
-        }
-    }
-
-    fn handle_top_project_bar(&mut self, ui: &mut Ui) {
-        ui.horizontal(|ui| {
-            for (tab, i) in self.tabs.iter().zip(0..) {
-                if ui.button(&tab.name).clicked() {
-                    self.current = i;
-                }
-            }
-
-            if ui.button("+").clicked() {
-                self.focus = Focus::Open
-            }
-        });
     }
 }
 
 impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| self.handle_top_project_bar(ui));
-
-        if self.focus == Focus::Open {
-            egui::Window::new("Add Project")
-                .anchor(egui::Align2::CENTER_CENTER, egui::vec2(0.0, 0.0))
-                .collapsible(false)
-                .resizable(false)
-                .show(ctx, |ui| self.handle_add_project_window(ui));
-        }
-
-        if self.tabs.is_empty() {
-            return;
-        }
-
-        if ctx.input(|i| i.events.iter().any(|ev| matches!(ev, egui::Event::Copy))) {
-            self.clipboard = self.tabs[self.current].get_selected();
-        }
-
-        if ctx.input(|i| i.events.iter().any(|ev| matches!(ev, egui::Event::Paste(_)))) {
-            self.tabs[self.current].add_objects(self.clipboard.clone());
-        }
-
-        if ctx.input(|i| i.modifiers.ctrl && i.key_released(egui::Key::S)) {
-            self.tabs[self.current].save();
-        }
-
-        self.tabs[self.current].handle_network_updates();
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            self.tabs[self.current].render(ui);
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            self.projects.render(ui)
         });
     }
 }
@@ -179,13 +43,13 @@ fn main() -> Result<(), error::Error> {
     let app = App::default();
 
     let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
+        viewport: ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
             .with_min_inner_size([300.0, 220.0]),
         ..Default::default()
     };
 
-    eframe::run_native("resync", native_options, Box::new(|_cc| Ok(Box::new(app))))?;
+    eframe::run_native("binal", native_options, Box::new(|_cc| Ok(Box::new(app))))?;
 
     Ok(())
 }
--- src/project.rs
+++ src/project.rs
@@ -2,16 +2,194 @@ use std::{
     collections::{HashMap, HashSet},
     fs::{File, OpenOptions},
     io::{Read, Write},
+    net::{Ipv4Addr, SocketAddrV4},
     path::{Path, PathBuf},
+    str::FromStr,
 };
 
 use eframe::egui::{self, Ui};
+use rfd::FileDialog;
 use serde::{Deserialize, Serialize};
 
 use crate::{
     error::Error,
     net::{Client, Message, Object},
+    UpdateWidget, Widget,
 };
+
+pub struct ProjectListing {
+    projects: Vec<Project>,
+    current: usize,
+
+    clipboard: ProjectData,
+
+    should_open: bool,
+    open_project: OpenProjectMenu,
+}
+
+impl Widget for ProjectListing {
+    fn render(&mut self, ui: &mut Ui) {
+        egui::TopBottomPanel::top("top_panel").show_inside(ui, |ui| {
+            ui.horizontal(|ui| {
+                for (tab, i) in self.projects.iter().zip(0..) {
+                    if ui.button(&tab.name).clicked() {
+                        self.current = i;
+                    }
+                }
+
+                if ui.button("+").clicked() {
+                    self.should_open = true;
+                }
+            });
+        });
+
+        if self.should_open {
+            egui::Window::new("Add Project")
+                .anchor(egui::Align2::CENTER_CENTER, egui::vec2(0.0, 0.0))
+                .collapsible(false)
+                .resizable(false)
+                .show(ui.ctx(), |ui| {
+                    self.open_project
+                        .render(ui, (&mut self.projects, &mut self.should_open));
+                });
+        }
+
+        if self.projects.is_empty() {
+            return;
+        }
+
+        self.projects[self.current].handle_network_updates();
+        self.projects[self.current].render(ui);
+
+        if ui.input(|i| i.events.iter().any(|ev| matches!(ev, egui::Event::Copy))) {
+            self.clipboard = self.projects[self.current].get_selected();
+        }
+
+        if ui.input(|i| {
+            i.events
+                .iter()
+                .any(|ev| matches!(ev, egui::Event::Paste(_)))
+        }) {
+            self.projects[self.current].add_objects(self.clipboard.clone());
+        }
+
+        if ui.input(|i| i.modifiers.ctrl && i.key_released(egui::Key::S)) {
+            self.projects[self.current].save();
+        }
+    }
+}
+
+impl Default for ProjectListing {
+    fn default() -> Self {
+        Self {
+            projects: Vec::new(),
+            current: 0,
+
+            clipboard: ProjectData::new(),
+
+            should_open: false,
+            open_project: OpenProjectMenu::default(),
+        }
+    }
+}
+
+struct OpenProjectMenu {
+    ip_text: String,
+    port_text: String,
+}
+
+impl<'a> UpdateWidget<'a> for OpenProjectMenu {
+    type State = (&'a mut Vec<Project>, &'a mut bool);
+
+    fn render(&mut self, ui: &mut Ui, state: Self::State) {
+        if ui.button("Close").clicked() {
+            *state.1 = false
+        }
+
+        ui.add_space(20.0);
+
+        //Open project from file
+        if ui.button("Open").clicked() {
+            let Some(file) = FileDialog::new().pick_file() else {
+                return;
+            };
+
+            let Some(filename) = file.file_name() else {
+                return;
+            };
+            let filename = filename.to_string_lossy().to_string();
+
+            let project = match Project::new(ProjectKind::Local(file), filename) {
+                Ok(project) => project,
+                Err(e) => {
+                    println!("{e}");
+                    return;
+                }
+            };
+
+            state.0.push(project);
+            *state.1 = false;
+        }
+
+        //Create new project
+        ui.add_space(5.0);
+
+        if ui.button("New").clicked() {
+            let Some(file) = FileDialog::new().save_file() else {
+                return;
+            };
+
+            let Some(filename) = file.file_name() else {
+                return;
+            };
+            let filename = filename.to_string_lossy().to_string();
+
+            let Ok(project) = Project::new(ProjectKind::Local(file), filename) else {
+                return;
+            };
+
+            state.0.push(project);
+            *state.1 = false;
+        }
+
+        // Open project with connection to client
+        ui.add_space(15.0);
+
+        let ip_label = ui.label("IP Address:");
+        ui.add(egui::TextEdit::singleline(&mut self.ip_text).hint_text("127.0.0.1"))
+            .labelled_by(ip_label.id);
+
+        let port_label = ui.label("Port:");
+        ui.add(egui::TextEdit::singleline(&mut self.port_text).hint_text("12007"))
+            .labelled_by(port_label.id);
+
+        if ui.button("Connect").clicked() {
+            let ip = Ipv4Addr::from_str(&self.ip_text).unwrap_or(Ipv4Addr::new(127, 0, 0, 1));
+            let port = u16::from_str(&self.port_text).unwrap_or(12007);
+
+            let Ok(client) = Client::connect(SocketAddrV4::new(ip, port)) else {
+                return;
+            };
+
+            let Ok(project) = Project::new(ProjectKind::Remote(client), self.ip_text.clone())
+            else {
+                return;
+            };
+
+            state.0.push(project);
+            *state.1 = false
+        }
+    }
+}
+
+impl Default for OpenProjectMenu {
+    fn default() -> Self {
+        Self {
+            ip_text: String::new(),
+            port_text: String::new(),
+        }
+    }
+}
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct ProjectData {
@@ -119,29 +297,36 @@ impl Project {
                 let index = match self.lookup.get(&name) {
                     Some(index) => *index,
                     None => {
-                        log::error!("Received delete message for object that does not exist in project: {}", name);
-                        return
+                        log::error!(
+                            "Received delete message for object that does not exist in project: {}",
+                            name
+                        );
+                        return;
                     }
                 };
-               
+
                 let last = self.data.objects.len() - 1;
                 if index != last {
                     self.data.objects.swap(index, last);
                     self.data.objects.pop();
                 }
-               
-                self.lookup.insert(self.data.objects[index].name.clone(), index);
+
+                self.lookup
+                    .insert(self.data.objects[index].name.clone(), index);
                 self.lookup.remove(&name);
             }
             Message::Rename { old, new } => {
                 let index = match self.lookup.get(&old) {
                     Some(index) => *index,
                     None => {
-                        log::error!("Received rename message for object that does not exist in project: {}", old);
-                        return
+                        log::error!(
+                            "Received rename message for object that does not exist in project: {}",
+                            old
+                        );
+                        return;
                     }
                 };
-               
+
                 self.data.objects[index].name = new.clone();
                 self.lookup.remove(&old);
                 self.lookup.insert(new, index);
@@ -150,35 +335,6 @@ impl Project {
                 self.data.objects.append(&mut objects);
             }
         }
-    }
-
-    // call to render the project to the UI and update the project state
-    pub fn render(&mut self, ui: &mut Ui) {
-        if ui.input(|i| i.key_released(egui::Key::Escape)) {
-            self.selected.clear()
-        }
-
-        ui.columns(2, |ui| {
-            let text_style = ui[0].text_style_height(&egui::TextStyle::Body);
-
-            egui::ScrollArea::vertical().show_rows(
-                &mut ui[0],
-                text_style,
-                self.data.objects.len(),
-                |ui, row_range| {
-                    for i in row_range {
-                        let selected = self.selected.contains(&i);
-                        let label = ui.selectable_label(selected, &self.data.objects[i].name);
-
-                        if label.clicked() && selected {
-                            self.selected.remove(&i);
-                        } else if label.clicked() && !selected {
-                            self.selected.insert(i);
-                        }
-                    }
-                },
-            );
-        });
     }
 
     // Get all objects that are selected at the moment in the project listing
@@ -214,5 +370,35 @@ impl Project {
                 objects: data.objects,
             });
         }
+    }
+}
+
+impl Widget for Project {
+    fn render(&mut self, ui: &mut Ui) {
+        if ui.input(|i| i.key_released(egui::Key::Escape)) {
+            self.selected.clear()
+        }
+
+        ui.columns(2, |ui| {
+            let text_style = ui[0].text_style_height(&egui::TextStyle::Body);
+
+            egui::ScrollArea::vertical().show_rows(
+                &mut ui[0],
+                text_style,
+                self.data.objects.len(),
+                |ui, row_range| {
+                    for i in row_range {
+                        let selected = self.selected.contains(&i);
+                        let label = ui.selectable_label(selected, &self.data.objects[i].name);
+
+                        if label.clicked() && selected {
+                            self.selected.remove(&i);
+                        } else if label.clicked() && !selected {
+                            self.selected.insert(i);
+                        }
+                    }
+                },
+            );
+        });
     }
 }
